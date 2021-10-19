6096c33819346e36d6827c0e391aa3e13bd3bce1
George Jose <jgeorge97@gmail.com>
2021-10-19 18:06:29 +0530

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,37 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <b-navbar type="is-primary">
+        <template #brand>
+            <b-navbar-item tag="router-link" :to="{ path: '/' }">
+                Dam Water Level Status
+            </b-navbar-item>
+        </template>
+        <template #start>
+            <b-navbar-item href="#">
+                Home
+            </b-navbar-item>
+            <b-navbar-dropdown label="Info">
+                <b-navbar-item href="#">
+                    About
+                </b-navbar-item>
+                <b-navbar-item href="#">
+                    Contact
+                </b-navbar-item>
+            </b-navbar-dropdown>
+        </template>
+      </b-navbar>
     </div>
     <router-view />
+    <div id="footer">
+      <footer class="footer">
+        <div class="content has-text-centered">
+            <p>
+              <strong>Dam Water Level Status App</strong> by <a href="https://jgthms.com">George Martin Jose</a>. The source code is listed on <a href="https://github.com/jgeorge97">Github</a>. Data by <a href="https://sdma.kerala.gov.in/dam-water-level/">KSDMA</a>.
+            </p>
+          </div>
+        </footer>
+    </div>
   </div>
 </template>
 
@@ -15,18 +42,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,62 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <div class="mt-5 has-text-info">
+      <h2>Last Updated on : {{ lastUpdated }}</h2>
+    </div>
+    <div class="columns is-centered">
+      <div class="column is-half">
+        <div class="card" v-for="dam in damData" v-bind:key="dam.slNo">
+          <header class="card-header">
+            <p class="card-header-title">
+              {{ dam.name }}
+            </p>
+          </header>
+          <div class="card-content">
+            <div class="content">
+              <p>Current Water Level: {{ dam.currentWaterLevel }}</p>
+              <p>Maximum allowed Water Level for today: {{ dam.currentDayMaxWaterLevel || "N/A" }}</p>
+              <p>Dam Maximum Capacity: {{ dam.maxWaterLevel }}</p> 
+              <p v-if="dam.remarks.length">Remarks: {{ dam.remarks }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <b-loading :is-full-page="true" v-model="isLoading" :can-cancel="true"></b-loading>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      damData: [],
+      lastUpdated: "",
+      isLoading: false,
+    }
   },
+  components: {
+  },
+  mounted() {
+    this.fetchData()
+  },
+  methods: {
+    async fetchData() {
+      this.isLoading = true
+      let res = await fetch(process.env.VUE_APP_API_URL + '/dam/getData')
+      let result = await res.json()
+      this.damData = result.message
+      this.lastUpdated = result.last_updated
+      this.isLoading = false
+    }
+  }
 };
 </script>
+<style>
+  .card {
+    margin-top: 0.45rem;
+  }
+</style>
