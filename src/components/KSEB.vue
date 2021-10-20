3ec84e413830d7f37b666f285423d7c9b823a725
George Jose <jgeorge97@gmail.com>
2021-10-20 12:52:48 +0530

    Fixed dam name in kseb

--- src/components/KSEB.vue
+++ src/components/KSEB.vue
@@ -8,7 +8,7 @@
         <div class="card" v-for="dam in damData" v-bind:key="dam.slNo">
           <header class="card-header">
             <p class="card-header-title is-size-5">
-              {{ dam.name }}
+              {{ dam.name.raw }}
             </p>
           </header>
           <div class="card-content">
